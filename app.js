3c1af027b508a9343a0c1b2765dfeb0fc76aa6d1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-29 14:53:03 -0500

    create app.js with expres and mysql, connect db

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connect to database
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "",
+});
+
+const connectedToDataBase = async () => {
+  try {
+    await dbConnection.connect();
+    console.log(`Connected to the database.`);
+  } catch (err) {
+    console.error("Database connection failed: " + err.stack);
+  }
+};
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
