d9066a9c81287cb415c6d96e0ee3ef72a00fed77
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-29 18:09:46 -0500

    modify index.js file by splitting prompts and switch cases in two different functions

--- js/index.js
+++ js/index.js
@@ -2,6 +2,36 @@ const inquirer = require("inquirer");
 
 const { app, dbConnection, connectedToDataBase } = require("../app.js");
 
+// Function to handle each action based on the user's choice
+const handleAction = async (action) => {
+  switch (action) {
+    case "View all departments":
+      // Handle the view all departments logic
+      break;
+    case "View all roles":
+      // Handle the view all roles logic
+      break;
+    case "View all employees":
+      // Handle the view all employees logic
+      break;
+    case "Add a department":
+      // Handle the add department logic
+      break;
+    case "Add a role":
+      // Handle the add role logic
+      break;
+    case "Add an employee":
+      // Handle the add employee logic
+      break;
+    case "Update an employee role":
+      // Handle the update employee role logic
+      break;
+    default:
+      console.log("Invalid choice. Please choose a valid option.");
+  }
+};
+
+// Main menu prompts
 const mainMenuPrompts = async () => {
   try {
     await connectedToDataBase();
@@ -22,33 +52,11 @@ const mainMenuPrompts = async () => {
       },
     ]);
 
-    switch (answers.menuChoiceToPick) {
-      case "View all departments":
-        // Handle the view all departments logic
-        break;
-      case "View all roles":
-        // Handle the view all roles logic
-        break;
-      case "View all employees":
-        // Handle the view all employees logic
-        break;
-      case "Add a department":
-        // Handle the add department logic
-        break;
-      case "Add a role":
-        // Handle the add role logic
-        break;
-      case "Add an employee":
-        // Handle the add employee logic
-        break;
-      case "Update an employee role":
-        // Handle the update employee role logic
-        break;
-      default:
-        console.log("Invalid choice. Please choose a valid option.");
-    }
+    await handleAction(answers.menuChoiceToPick);
   } catch (err) {
     console.error("Error:", err);
+  } finally {
+    await dbConnection.end();
   }
 };
 
